ac1818708b3a08756eceb3f37cf2367011df80f2
developerkamol <developerkamol@gmail.com>
2025-03-29 17:51:33 +0500

    more structured, sorted data

--- iExpenseTracker/ContentView.swift
+++ iExpenseTracker/ContentView.swift
@@ -7,6 +7,63 @@
 
 import SwiftUI
 
+struct ExpenseRow: View {
+    let item: ExpenseItem
+
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(.headline)
+                Text(item.type)
+            }
+            Spacer()
+            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+        }
+        .frame(maxWidth: .infinity)
+        .listRowBackground(amountColor(item.amount).opacity(0.2))
+        .foregroundColor(amountColor(item.amount))
+        .fontWeight(amountWeight(item.amount))
+        .cornerRadius(8)
+    }
+
+    func amountColor(_ amount: Double) -> Color {
+        if amount < 10 {
+            return .green
+        } else if amount < 100 {
+            return .orange
+        } else {
+            return .red
+        }
+    }
+
+    func amountWeight(_ amount: Double) -> Font.Weight {
+        if amount < 10 {
+            return .light
+        } else if amount < 100 {
+            return .medium
+        } else {
+            return .bold
+        }
+    }
+}
+
+struct ExpenseSection: View {
+    let title: String
+    let items: [ExpenseItem]
+    let removeAction: (IndexSet) -> Void
+
+    var body: some View {
+        Section(title) {
+            ForEach(items, id: \.id) { item in
+                ExpenseRow(item: item)
+            }
+            .onDelete(perform: removeAction)
+        }
+    }
+}
+
+
 struct ExpenseItem: Identifiable, Codable {
     var id = UUID()
     let name: String
@@ -43,18 +100,8 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             List {
-                ForEach(expenses.items, id: \.id){ item in
-                    HStack{
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
-                        }
-                            Spacer()
-                            
-                        Text(item.amount, format: .currency(code: "USD"))
-                    }
-                } .onDelete(perform: removeItems)
+                ExpenseSection(title: "Personal expenses", items: expenses.items.filter { $0.type == "Personal" }, removeAction: removeItems)
+                ExpenseSection(title: "Business expenses", items: expenses.items.filter { $0.type == "Business" }, removeAction: removeItems)
             }
             .navigationTitle("iExpenseTracker")
             .toolbar{
