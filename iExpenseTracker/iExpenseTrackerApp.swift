0857f8c4ebb692d94f2a0a9be30a1e1953f16cac
developerkamol <developerkamol@gmail.com>
2025-03-29 14:36:12 +0500

    Initial Commit

---
+++ iExpenseTracker/iExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseTrackerApp.swift
+//  iExpenseTracker
+//
+//  Created by Kamol Madaminov on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
